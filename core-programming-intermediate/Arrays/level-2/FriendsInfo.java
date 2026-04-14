c99f68845eae23fa55976ee36c1c0197c8a7110f
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2026-04-14 22:53:29 +0530

    Employeebonus and BMIarray

---
+++ core-programming-intermediate/Arrays/level-2/FriendsInfo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class FriendsInfo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            age[i] = sc.nextInt();
+
+            System.out.print("Enter height of " + names[i] + ": ");
+            height[i] = sc.nextDouble();
+        }
+
+        int minAgeIndex = 0;
+        int maxHeightIndex = 0;
+
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[minAgeIndex]) {
+                minAgeIndex = i;
+            }
+
+            if (height[i] > height[maxHeightIndex]) {
+                maxHeightIndex = i;
+            }
+        }
+
+        System.out.println("Youngest Friend: " + names[minAgeIndex]);
+        System.out.println("Tallest Friend: " + names[maxHeightIndex]);
+
+        sc.close();
+    }
+}
