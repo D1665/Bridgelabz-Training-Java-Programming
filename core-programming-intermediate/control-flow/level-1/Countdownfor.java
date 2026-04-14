9d5dc3111096d8284839b38d9c4d99492fa2caea
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2026-04-14 12:42:46 +0530

    countdownfor

---
+++ core-programming-intermediate/control-flow/level-1/Countdownfor.java
@@ -0,0 +1,11 @@
+
+public class Countdownfor {
+    public static void main(String[] args) {
+        int start = 10; // Starting point of the countdown
+
+        System.out.println("Countdown:");
+        for (int i = start; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
