1fe060714b669f045598b9bf4b5325090f82328e
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2026-04-18 15:44:27 +0530

    Student grade System

--- StudentGradeSystem.java
+++ StudentGradeSystem.java
@@ -16,30 +16,35 @@ public class StudentGradeSystem {
        int elecnum=sc.nextInt();
        System.out.println("Enter your Chemistry num =");
        int chemnum=sc.nextInt();
-       int average=(int)((mathnum+engnum+phynum+elecnum+chemnum)/500);
+       int sum=mathnum+engnum+phynum+elecnum+chemnum;
+       sum=sum*100;
+       int average=sum/500;
+
+       System.out.println("Your all over percentage is :"+average);
+
        if(average<33){
-        System.out.println("Sorry! , Your are failed.");
+        System.out.println("Sorry! "+name+", Your are failed.");
        }
        else if(average>=33&&average<=40){
-        System.out.println("Congrats , You are passed and got C grade");
+        System.out.println("Congrats " +name+ ", You are passed and got C grade");
        }
        else if(average>40&&average<=50){
-        System.out.println("Congrats , You are passed and got B- grade");
+        System.out.println("Congrats "+name+", You are passed and got B- grade");
        }
        else if(average>50&&average<=60){
-        System.out.println("Congrats , You are passed and got B grade");
+        System.out.println("Congrats "+name+" , You are passed and got B grade");
        }
        else if(average>60&&average<=70){
-        System.out.println("Congrats , You are passed and got B+ grade");
+        System.out.println("Congrats "+name+", You are passed and got B+ grade");
        }
        else if(average>70&&average<=80){
-        System.out.println("Congrats , You are passed and got A grade");
+        System.out.println("Congrats "+name+", You are passed and got A grade");
        }
        else if(average>80&&average<=90){
-        System.out.println("Congrats , You are passed and got A+ grade");
+        System.out.println("Congrats "+name+" , You are passed and got A+ grade");
        }
        else if(average>90&&average<=100){
-        System.out.println("Congrats , You are passed and got O grade");
+        System.out.println("Congrats "+name+", You are passed and got O grade");
        }
 
     }
