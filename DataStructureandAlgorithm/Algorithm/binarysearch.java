8879ba29a2035cb977cd77345750d4fd59ad44cf
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2026-05-14 12:40:45 +0530

    dsa

---
+++ DataStructureandAlgorithm/Algorithm/binarysearch.java
@@ -0,0 +1,5 @@
+package DataStructureandAlgorithm.Algorithm;
+
+public class binarysearch {
+    
+}
---
+++ DataStructureandAlgorithm/Data_Structure/Arrays.java
@@ -0,0 +1,5 @@
+package DataStructureandAlgorithm.Data_Structure;
+
+public class Arrays {
+    
+}
